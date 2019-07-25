14b23026b40ad434b8b3853892139383471f1066
get4137 <get4137@gmail.com>
2019-07-25 12:30:11 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,40 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Loader from './Loader';
+import App from './components/App'
 
-class App extends React.Component {
-
-    state = { lat: null, errorMessage: '' };
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({lat: position.coords.latitude}),
-            err => this.setState({ errorMessage: err.message})
-        );
-    }
-
-    renderContent() {
-  if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
-  }
-
-  if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
-  }
-  return <div><Loader message="Please accept location request" /></div>;
-    }
-
-    render() {
-        return (
-            <div className="for_example">
-                {this.renderContent()}
-            </div>
-         )
-    }
-
-}
-
-ReactDOM.render(
-    <App />,
+ReactDOM.render(<App />,
     document.querySelector('#root')
 );
